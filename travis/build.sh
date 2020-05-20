f33455df57166a5b0a151a68ddad1b355f0b861b
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-05-20 13:22:26 +0200

    travis

--- travis/build.sh
+++ travis/build.sh
@@ -11,6 +11,11 @@ ls
 
 make install
 
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/test/requirements.txt
+    "${PYBIN}/pytest"
+done
+
 mkdir -p /io/build
-cp -R bin /io/build/
+cp -R lib /io/build/
 cp -R share /io/build/

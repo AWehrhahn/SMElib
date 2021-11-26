bf18b1a6e3b13dc3d000422c98d11a7a7791dd90
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-11-26 18:13:58 +0100

    make the test work in debugging from a different directory

--- pymodule/test_pymodule.py
+++ pymodule/test_pymodule.py
@@ -1,3 +1,11 @@
+
+import os
+from os.path import dirname, abspath
+libdir = abspath(dirname(__file__))
+try:
+    os.add_dll_directory(libdir)
+except AttributeError:
+    os.chdir(libdir)
 import _smelib
 
 print(_smelib.LibraryVersion())
@@ -7,3 +15,6 @@ print(_smelib.GetLibraryPath())
 print(_smelib.SetLibraryPath("/bla/"))
 print(_smelib.GetLibraryPath())
 print(_smelib.InputWaveRange(1000, 2000))
+
+# print(_smelib.InputWaveRange(1000, "2000"))
+

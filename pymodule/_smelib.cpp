804795562273ebe3be483655ac2baf971be16f33
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-11-29 18:17:47 +0100

    More PyModule interface

--- pymodule/_smelib.cpp
+++ pymodule/_smelib.cpp
@@ -138,7 +138,541 @@ static PyObject * smelib_SetVWscale(PyObject * self, PyObject *args)
     Py_RETURN_NONE;
 }
 
+static char smelib_SetH2broad_docstring[] = "Set flag for H2 molecule";
+static PyObject * smelib_SetH2broad(PyObject * self, PyObject *args)
+{
+    int n = 0;
+    const char * result = NULL;
+    void ** args_c = NULL;
 
+    result = SetH2broad(n, args_c);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+static char smelib_ClearH2broad_docstring[] = "Clear flag for H2 molecule";
+static PyObject * smelib_ClearH2broad(PyObject * self, PyObject *args)
+{
+    int n = 0;
+    const char * result = NULL;
+    void ** args_c = NULL;
+
+    result = ClearH2broad(n, args_c);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+static char smelib_InputLineList_docstring[] = "Read in line list";
+static PyObject * smelib_InputLineList(PyObject * self, PyObject *args)
+{
+    const int n = 3;
+    const char * result = NULL;
+    void * args_c[n];
+    int nlines = 0, nchar = 0;
+    PyObject * species_obj = NULL, * linelist_obj = NULL;
+    PyArrayObject * species_array = NULL, *linelist_array = NULL;
+    IDL_STRING * species = NULL;
+    char * species_data = NULL;
+    double * linelist = NULL;
+    PyArray_Descr * dtype = NULL;
+
+
+    if (!PyArg_ParseTuple(args, "OO", &species_obj, &linelist_obj))
+        return NULL;
+
+    // Convert to Numpy arrays
+    species_array = (PyArrayObject *) PyArray_FROM_OTF(species_obj, NPY_STRING, NPY_ARRAY_IN_ARRAY);
+    linelist_array = (PyArrayObject *) PyArray_FROM_OTF(linelist_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+
+    
+    // Check if that worked
+    if (species_array == NULL || linelist_array == NULL){
+        goto fail;
+    }
+
+    // Check dimensions
+    if (PyArray_NDIM(species_array) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected species array of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(linelist_array) != 2){
+        PyErr_SetString(PyExc_ValueError, "Expected linelist of ndim == 2");
+        goto fail;
+    }
+    // Get sizes
+    nlines = PyArray_DIM(species_array, 0);
+    dtype = PyArray_DESCR(species_array);
+    nchar = dtype->elsize;
+
+    if (PyArray_DIM(linelist_array, 0) != 8){
+        PyErr_SetString(PyExc_ValueError, "Expected linelist to have 8 values");
+        goto fail;
+    }
+    if (PyArray_DIM(linelist_array, 1) != nlines){
+        PyErr_SetString(PyExc_ValueError, "Expected both arrays to have the same length");
+        goto fail;
+    }
+
+    // Get the pointers to pass to SMELIB
+    linelist = (double*) PyArray_DATA(linelist_array);
+    species_data = (char*) PyArray_DATA(species_array);
+    species = (IDL_STRING *) malloc(nlines * sizeof(IDL_STRING));
+
+    for (int i = 0; i < nlines; i++)
+    {
+        species[i].slen = nchar;
+        species[i].s = &species_data[nchar*i];
+        species[i].stype = 0;
+    }
+  
+    // printf("0: %s\n", species[0].s);
+    args_c[0] = &nlines;
+    args_c[1] = species;
+    args_c[2] = linelist;
+    result = InputLineList(n, args_c);
+
+
+    free(species);
+    Py_DECREF(species_array);
+    Py_DECREF(linelist_array);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+
+fail:
+    Py_XDECREF(species_array);
+    Py_XDECREF(linelist_array);
+    return NULL;
+}
+
+
+static char smelib_OutputLineList_docstring[] = "Return line list";
+static PyObject * smelib_OutputLineList(PyObject * self, PyObject *args)
+{
+    const int n = 2;
+    const char * result = NULL;
+    void * args_c[n];
+    int nlines;
+    double * linelist = NULL;
+    PyArrayObject * linelist_array;
+
+    nlines = GetNLINES();
+
+    npy_intp dims[2] = {nlines, 6};
+    linelist_array = (PyArrayObject*) PyArray_SimpleNew(2, dims, NPY_DOUBLE);
+    linelist = (double*) PyArray_DATA(linelist_array);
+
+    args_c[0] = &nlines;
+    args_c[1] = linelist;
+    result = OutputLineList(n, args_c);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        Py_DecRef((PyObject*)linelist_array);
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    return (PyObject*) linelist_array;
+}
+
+static char smelib_UpdateLineList_docstring[] = "Change line list parameters";
+static PyObject * smelib_UpdateLineList(PyObject * self, PyObject *args)
+{
+    const int n = 4;
+    const char * result = NULL;
+    void * args_c[n];
+    int nlines, nchar;
+    double * linelist = NULL;
+    IDL_STRING * species = NULL;
+    short * index = NULL;
+    PyObject * linelist_obj = NULL, * species_obj = NULL, * index_obj = NULL;
+    PyArrayObject * linelist_array = NULL, * species_array = NULL, * index_array = NULL;
+    char * species_data = NULL;
+    PyArray_Descr * dtype = NULL;
+    
+    
+    if (!PyArg_ParseTuple(args, "OOO", &species_obj, &linelist_obj, &index_obj))
+        return NULL;
+
+    // Convert to Numpy arrays
+    species_array = (PyArrayObject *) PyArray_FROM_OTF(species_obj, NPY_STRING, NPY_ARRAY_IN_ARRAY);
+    linelist_array = (PyArrayObject *) PyArray_FROM_OTF(linelist_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    index_array = (PyArrayObject *) PyArray_FROM_OTF(index_obj, NPY_SHORT, NPY_ARRAY_IN_ARRAY);
+
+    if (species_array == NULL || linelist_array == NULL || index_array == NULL){
+        goto fail;
+    }
+
+     // Check dimensions
+    if (PyArray_NDIM(species_array) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected species array of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(linelist_array) != 2){
+        PyErr_SetString(PyExc_ValueError, "Expected linelist of ndim == 2");
+        goto fail;
+    }
+    if (PyArray_NDIM(index_array) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected index array of ndim == 1");
+        goto fail;
+    }
+
+    // Get sizes
+    nlines = PyArray_DIM(species_array, 0);
+    dtype = PyArray_DESCR(species_array);
+    nchar = dtype->elsize;
+
+    // Check sizes
+    if (PyArray_DIM(linelist_array, 0) != 8){
+        PyErr_SetString(PyExc_ValueError, "Expected linelist to have 8 values");
+        goto fail;
+    }
+    if (PyArray_DIM(linelist_array, 1) != nlines){
+        PyErr_SetString(PyExc_ValueError, "Expected all arrays to have the same length");
+        goto fail;
+    }
+    if (PyArray_DIM(index_array, 0) != nlines){
+        PyErr_SetString(PyExc_ValueError, "Expected all arrays to have the same length");
+        goto fail;
+    }
+
+    // Get the pointers to pass to SMELIB
+    linelist = (double*) PyArray_DATA(linelist_array);
+    species_data = (char*) PyArray_DATA(species_array);
+    index = (short *) PyArray_DATA(index_array);
+    species = (IDL_STRING *) malloc(nlines * sizeof(IDL_STRING));
+
+    for (int i = 0; i < nlines; i++)
+    {
+        species[i].slen = nchar;
+        species[i].s = &species_data[nchar*i];
+        species[i].stype = 0;
+    }
+
+    args_c[0] = &nlines;
+    args_c[1] = species;
+    args_c[2] = linelist;
+    args_c[3] = index;
+    result = UpdateLineList(n, args_c);
+
+    free(species);
+    Py_XDECREF(species_array);
+    Py_XDECREF(linelist_array);
+    Py_XDECREF(index_array);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+
+fail:
+    Py_XDECREF(species_array);
+    Py_XDECREF(linelist_array);
+    Py_XDECREF(index_array);
+    return NULL;
+}
+
+static char smelib_InputModel_docstring[] = "Read in model atmosphere";
+static PyObject * smelib_InputModel(PyObject * self, PyObject *args, PyObject *kwds)
+{
+    int n = 14;
+    void * args_c[14];
+    const char * result = NULL;
+    double teff, grav, wlstd, radius = NAN;
+    char * motype;
+    IDL_STRING motype_idl;
+    int nrhox;
+    PyObject * opflag_obj = NULL, *depth_obj = NULL, *temp_obj = NULL;
+    PyObject * xne_obj = NULL, *xna_obj = NULL, *rho_obj = NULL, *vt_obj = NULL;
+    PyObject * height_obj = NULL;
+
+    PyArrayObject * opflag_arr = NULL, *depth_arr = NULL, *temp_arr = NULL;
+    PyArrayObject * xne_arr = NULL, *xna_arr = NULL, *rho_arr = NULL, *vt_arr = NULL;
+    PyArrayObject * height_arr = NULL;
+
+    // Need to make this constant because C++ needs it
+    // but we cast to non constant and trust Python
+    static const char * keywords[] = {"teff", "grav", "wlstd", "motype", "opflag", "depth", "temp", "xna", "xne", "rho", "vt", "radius", "height", NULL};
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "dddsOOOOOOO|dO", const_cast<char **>(keywords), &teff, &grav, &wlstd, &motype, 
+            &opflag_obj, &depth_obj, &temp_obj, &xna_obj, &xne_obj, &rho_obj, &vt_obj, &radius, &height_obj))
+        return NULL;
+
+    // Convert to Numpy arrays
+    opflag_arr = (PyArrayObject *) PyArray_FROM_OTF(opflag_obj, NPY_SHORT, NPY_ARRAY_IN_ARRAY);
+    depth_arr = (PyArrayObject *) PyArray_FROM_OTF(depth_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    temp_arr = (PyArrayObject *) PyArray_FROM_OTF(temp_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    xna_arr = (PyArrayObject *) PyArray_FROM_OTF(xna_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    xne_arr = (PyArrayObject *) PyArray_FROM_OTF(xne_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    rho_arr = (PyArrayObject *) PyArray_FROM_OTF(rho_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    vt_arr = (PyArrayObject *) PyArray_FROM_OTF(vt_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+
+    if (opflag_arr == NULL || depth_arr == NULL || temp_arr == NULL || xna_arr == NULL ||
+        xne_arr == NULL || rho_arr == NULL || vt_arr == NULL){
+            goto fail;
+        }
+
+    // Only if given
+    if (height_obj != NULL){
+        height_arr = (PyArrayObject *) PyArray_FROM_OTF(height_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+        if (height_arr == NULL){
+            goto fail;
+        }
+    }
+
+    if (PyArray_NDIM(opflag_arr) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected opflag array of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(depth_arr) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected depth of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(temp_arr) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected temp array of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(xna_arr) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected XNA array of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(xne_arr) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected XNE array of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(rho_arr) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected rho array of ndim == 1");
+        goto fail;
+    }
+    if (PyArray_NDIM(vt_arr) != 1){
+        PyErr_SetString(PyExc_ValueError, "Expected v_turb array of ndim == 1");
+        goto fail;
+    }
+    if (height_arr != NULL){
+        if (PyArray_NDIM(height_arr) != 1){
+            PyErr_SetString(PyExc_ValueError, "Expected height array of ndim == 1");
+            goto fail;
+        }
+    }
+
+    // Get size
+    nrhox = PyArray_DIM(depth_arr, 0);
+
+    // Check size
+    if (PyArray_DIM(opflag_arr, 0) != 20){
+        PyErr_SetString(PyExc_ValueError, "Expected opflag array of size 20");
+        goto fail;
+    }
+    if (PyArray_DIM(depth_arr, 0) != nrhox){
+        PyErr_SetString(PyExc_ValueError, "Expected depth of the same size");
+        goto fail;
+    }
+    if (PyArray_DIM(temp_arr, 0) != nrhox){
+        PyErr_SetString(PyExc_ValueError, "Expected temp array of the same size");
+        goto fail;
+    }
+    if (PyArray_DIM(xna_arr, 0) != nrhox){
+        PyErr_SetString(PyExc_ValueError, "Expected XNA array of the same size");
+        goto fail;
+    }
+    if (PyArray_DIM(xne_arr, 0) != nrhox){
+        PyErr_SetString(PyExc_ValueError, "Expected XNE array of the same size");
+        goto fail;
+    }
+    if (PyArray_DIM(rho_arr, 0) != nrhox){
+        PyErr_SetString(PyExc_ValueError, "Expected rho array of the same size");
+        goto fail;
+    }
+    if (PyArray_DIM(vt_arr, 0) != nrhox){
+        PyErr_SetString(PyExc_ValueError, "Expected v_turb array of the same size");
+        goto fail;
+    }
+    if (height_arr != NULL){
+        if (PyArray_DIM(height_arr, 0) != nrhox){
+            PyErr_SetString(PyExc_ValueError, "Expected height array of the same size");
+            goto fail;
+        }
+    }
+
+    // Convert to IDL string
+    motype_idl.s = motype;
+    motype_idl.slen = strlen(motype);
+    motype_idl.stype = 0;
+
+    if (strncmp(motype, "SPH", 3) == 0){
+        if (isnan(radius) || height_arr == NULL){
+            PyErr_SetString(PyExc_ValueError, "Model type is SPH but no height and/or radius are given");
+            goto fail;
+        }
+        args_c[0] = &nrhox;
+        args_c[1] = &teff;
+        args_c[2] = &grav;
+        args_c[3] = &wlstd;
+        args_c[4] = &motype_idl;
+        args_c[5] = &radius;
+        args_c[6] = (short*) PyArray_DATA(opflag_arr);
+        args_c[7] = (double*) PyArray_DATA(depth_arr);
+        args_c[8] = (double*) PyArray_DATA(temp_arr);
+        args_c[9] = (double*) PyArray_DATA(xne_arr);
+        args_c[10] = (double*) PyArray_DATA(xna_arr);
+        args_c[11] = (double*) PyArray_DATA(rho_arr);
+        args_c[12] = (double*) PyArray_DATA(vt_arr);
+        args_c[13] = (double*) PyArray_DATA(height_arr);
+        n = 14;
+    } else {
+        args_c[0] = &nrhox;
+        args_c[1] = &teff;
+        args_c[2] = &grav;
+        args_c[3] = &wlstd;
+        args_c[4] = &motype_idl;
+        args_c[5] = (short*) PyArray_DATA(opflag_arr);
+        args_c[6] = (double*) PyArray_DATA(depth_arr);
+        args_c[7] = (double*) PyArray_DATA(temp_arr);
+        args_c[8] = (double*) PyArray_DATA(xne_arr);
+        args_c[9] = (double*) PyArray_DATA(xna_arr);
+        args_c[10] = (double*) PyArray_DATA(rho_arr);
+        args_c[11] = (double*) PyArray_DATA(vt_arr);
+        n = 12;
+    }
+
+    result = InputModel(n, args_c);
+
+    Py_XDECREF(opflag_arr);
+    Py_XDECREF(depth_arr);
+    Py_XDECREF(temp_arr);
+    Py_XDECREF(xna_arr);
+    Py_XDECREF(xne_arr);
+    Py_XDECREF(rho_arr);
+    Py_XDECREF(vt_arr);
+    Py_XDECREF(height_arr);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+
+fail:
+    Py_XDECREF(opflag_arr);
+    Py_XDECREF(depth_arr);
+    Py_XDECREF(temp_arr);
+    Py_XDECREF(xna_arr);
+    Py_XDECREF(xne_arr);
+    Py_XDECREF(rho_arr);
+    Py_XDECREF(vt_arr);
+    Py_XDECREF(height_arr);
+    return NULL;
+}
+
+static char smelib_InputDepartureCoefficients_docstring[] = "Input Departure coefficients";
+static PyObject * smelib_InputDepartureCoefficients(PyObject * self, PyObject *args)
+{
+    int n = 2;
+    void * args_c[n];
+    const char * result = NULL;
+    int linenumber;
+    short nrhox;
+    PyObject * bmatrix_obj = NULL;
+    PyArrayObject * bmatrix_arr = NULL;
+
+    if (!PyArg_ParseTuple(args, "iO", &linenumber, &bmatrix_obj))
+        return NULL;
+
+    bmatrix_arr = (PyArrayObject *) PyArray_FROM_OTF(bmatrix_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+
+    if (bmatrix_arr == NULL){
+        return NULL;
+    }
+
+    nrhox = GetNRHOX();
+
+    if (PyArray_NDIM(bmatrix_arr) != 2){
+        Py_XDECREF(bmatrix_arr);
+        PyErr_SetString(PyExc_ValueError, "Expected bmatrix with ndim == 2");
+        return NULL;
+    }
+    if (PyArray_DIM(bmatrix_arr, 0) != 2){
+        Py_XDECREF(bmatrix_arr);
+        PyErr_SetString(PyExc_ValueError, "Expected bmatrix with shape (2, nrhox)");
+        return NULL;
+    }
+    if (PyArray_DIM(bmatrix_arr, 1) != nrhox){
+        Py_XDECREF(bmatrix_arr);
+        PyErr_SetString(PyExc_ValueError, "Expected bmatrix with shape (2, nrhox)");
+        return NULL;
+    }
+
+    args_c[0] = (double*) PyArray_DATA(bmatrix_arr);
+    args_c[1] = &linenumber;
+    result = InputDepartureCoefficients(n, args_c);
+
+    Py_DECREF(bmatrix_arr);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+static char smelib_GetDepartureCoefficients_docstring[] = "Get NLTE b's for specific line";
+static PyObject * smelib_GetDepartureCoefficients(PyObject * self, PyObject *args)
+{
+    const int n = 3;
+    void * args_c[n];
+    const char * result = NULL;
+    int nrhox, linenumber;
+
+    PyArrayObject * bmatrix_arr;
+    double * bmatrix_data;
+
+        
+    if (!PyArg_ParseTuple(args, "i", &linenumber))
+        return NULL;
+
+    nrhox = GetNRHOX();
+    npy_intp dims[2] = {2, nrhox};    
+    bmatrix_arr = (PyArrayObject*) PyArray_SimpleNew(2, dims, NPY_DOUBLE);
+    bmatrix_data = (double*) PyArray_DATA(bmatrix_arr);
+
+    args_c[0] = bmatrix_data;
+    args_c[1] = &nrhox;
+    args_c[2] = &linenumber;
+    result = GetDepartureCoefficients(n, args_c);
+
+    if (result != NULL && result[0] != OK_response)
+    {
+        Py_DecRef((PyObject*)bmatrix_arr);
+        PyErr_SetString(PyExc_RuntimeError, result);
+        return NULL;
+    }
+
+    return (PyObject*) bmatrix_arr;
+}
 
 
 static PyMethodDef module_methods[] = {
@@ -148,6 +682,14 @@ static PyMethodDef module_methods[] = {
     {"SetLibraryPath", smelib_SetLibraryPath, METH_VARARGS, smelib_SetLibraryPath_docstring},
     {"InputWaveRange", smelib_InputWaveRange, METH_VARARGS, smelib_InputWaveRange_docstring},
     {"SetVWscale", smelib_SetVWscale, METH_VARARGS, smelib_SetVWscale_docstring},
+    {"SetH2broad", smelib_SetH2broad, METH_VARARGS, smelib_SetH2broad_docstring},
+    {"ClearH2broad", smelib_ClearH2broad, METH_VARARGS, smelib_ClearH2broad_docstring},
+    {"InputLineList", smelib_InputLineList, METH_VARARGS, smelib_InputLineList_docstring},
+    {"OutputLineList", smelib_OutputLineList, METH_VARARGS, smelib_OutputLineList_docstring},
+    {"UpdateLineList", smelib_UpdateLineList, METH_VARARGS, smelib_UpdateLineList_docstring},
+    {"InputModel", (PyCFunction)(void(*)(void))smelib_InputModel, METH_VARARGS|METH_KEYWORDS, smelib_InputModel_docstring},
+    {"InputDepartureCoefficients", smelib_InputDepartureCoefficients, METH_VARARGS, smelib_InputDepartureCoefficients_docstring},
+    {"GetDepartureCoefficients", smelib_GetDepartureCoefficients, METH_VARARGS, smelib_GetDepartureCoefficients_docstring},
     {NULL, NULL, 0, NULL}
 };
 
--- pymodule/test_pymodule.py
+++ pymodule/test_pymodule.py
@@ -1,6 +1,8 @@
 
 import os
 from os.path import dirname, abspath
+import numpy as np
+
 libdir = abspath(dirname(__file__))
 try:
     os.add_dll_directory(libdir)
@@ -8,13 +10,40 @@ except AttributeError:
     os.chdir(libdir)
 import _smelib
 
-print(_smelib.LibraryVersion())
-print(_smelib.GetDataFiles())
-print(_smelib.GetLibraryPath())
+# print(_smelib.LibraryVersion())
+# print(_smelib.GetDataFiles())
+# print(_smelib.GetLibraryPath())
 
-print(_smelib.SetLibraryPath("/bla/"))
-print(_smelib.GetLibraryPath())
-print(_smelib.InputWaveRange(1000, 2000))
+# print(_smelib.SetLibraryPath("/bla/"))
+# print(_smelib.GetLibraryPath())
+# print(_smelib.InputWaveRange(1000, 2000))
+
+species = np.asarray(["Na 1", "Li 1"], dtype="S8")
+linelist = np.zeros((8, 2))
+linelist[3, :] = 1
+linelist[2, 0] = 100
+linelist[2, 1] = 1000
+
+_smelib.InputLineList(species, linelist)
+# _smelib.UpdateLineList(["Na 1"], linelist[:, 0:1], [0])
+# data = _smelib.OutputLineList()
+
+# print(linelist[2:])
+# print(data.T.shape)
+# print(data.T)
+
+opflag = np.zeros(20, dtype=np.int16)
+depth = temp = xna = xne = rho = vt = height = np.zeros(10)
+_smelib.InputModel(5000, 4.4, 5000, "RHOX", opflag, depth, temp, xna, xne, rho, vt, radius=-10)
+
+bmat = np.ones((2, 10))
+_smelib.InputDepartureCoefficients(0, bmat)
+
+data = _smelib.GetDepartureCoefficients(0)
+print(data)
+
+# _smelib.InputLineList(species, "bla")
+
 
 # print(_smelib.InputWaveRange(1000, "2000"))
 
